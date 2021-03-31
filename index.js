bfa940ccf06be2d8ea2b5f5f3dae265b7b6fecb0
Sarman7105 <sarman7105@gmail.com>
2021-03-31 22:28:22 +0600

    server post request working

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
-const app = express();
-const port = process.env.PORT || 3000;
+
+require('dotenv').config()
+
+const port = process.env.PORT || 5055;
+
 app.use(cors());
 app.use(bodyParser.json());
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0q0ko.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    console.log('error',err );
-    const bookCollection = client.db("book-shop").collection("books");
-    // bookCollection.insertOne('sochib');
-    console.log('database connected successfully');
-  // perform actions on the collection object
-//   client.close();
+	res.send('Hello World!');
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0q0ko.mongodb.net/${process.env
+	.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((err) => {
+	// console.log('error',err );
+	const bookCollection = client.db('book-shop').collection('books');
+	
+	app.post('/addBook', (req, res) => {
+      const newEvent = req.body;
+      console.log('adding new event: ', newEvent)
+      bookCollection.insertOne(newEvent)
+      .then(result => {
+          console.log('inserted count', result.insertedCount);
+          res.send(result.insertedCount > 0)
+      })
+  })
+	
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-    console.log(process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME);
-})
+	console.log(`Example app listening at http://localhost:${port}`);
+	// console.log(process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME);
+});
