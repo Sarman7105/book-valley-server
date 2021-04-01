23bd428ad42acf2d4eb068fcd437d1c0dfffcf37
Sarman7105 <sarman7105@gmail.com>
2021-04-01 23:12:37 +0600

    delete api created

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const port = process.env.PORT || 5055;
 
 app.use(cors());
 app.use(bodyParser.json());
-
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
@@ -19,36 +19,52 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0q0ko.mongodb.net/${process.env
 	.DB_NAME}?retryWrites=true&w=majority`;
 console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect((err) => {
 	// console.log('error',err );
 	const bookCollection = client.db('book-shop').collection('books');
 	const orderCollection = client.db('book-shop').collection('orders');
-	
+
 	app.post('/addBook', (req, res) => {
-      const newEvent = req.body;
-    //   console.log('adding new event: ', newEvent)
-      bookCollection.insertOne(newEvent)
-      .then(result => {
-          console.log('inserted count', result.insertedCount);
-          res.send(result.insertedCount > 0)
-      })
-	})
+		const newEvent = req.body;
+		//   console.log('adding new event: ', newEvent)
+		bookCollection.insertOne(newEvent).then((result) => {
+			console.log('inserted count', result.insertedCount);
+			res.send(result.insertedCount > 0);
+		});
+	});
+
+
 	app.post('/addOrder', (req, res) => {
-      const newOrder = req.body;
+		const newOrder = req.body;
 		console.log('adding new order: ', newOrder);
-      orderCollection.insertOne(newOrder)
-      .then(result => {
-          console.log('inserted count', result.insertedCount);
-          res.send(result.insertedCount > 0)
-      })
+		orderCollection.insertOne(newOrder).then((result) => {
+			console.log('inserted count', result.insertedCount);
+			res.send(result.insertedCount > 0);
+		});
+	});
+
+	app.delete('/deleteProduct/:id', (req, res) => {
+		const id = ObjectID(req.params.id);
+		console.log('delete this', id);
+		bookCollection.findOneAndDelete({ _id: id })
+		.then(documents=>res.send(documents.value))
 	})
+
 	app.get('/books', (req, res) => {
-		bookCollection.find().toArray((err,items) => {
+		bookCollection.find().toArray((err, items) => {
 			res.send(items);
-		})
-	})
-	
+		});
+	});
+
+
+	app.get('/orders', (req, res) => {
+		console.log(req.query.email);
+		orderCollection.find({ email: req.query.email }).toArray((err, items) => {
+			res.send(items);
+		});
+	});
 });
 
 app.listen(port, () => {
